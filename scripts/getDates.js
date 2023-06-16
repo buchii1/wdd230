aaf62e48dd7f6fe74bfed5aeafa8fb12385489fd
buchii1 <oko22007@byui.edu>
2023-06-16 14:08:50 +0100

    Modified the hamburger menu script

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -15,4 +15,10 @@ const navUl = document.querySelector(".nav-ul");
 menuBtn.addEventListener("click", () => {
 	navUl.classList.toggle("open");
 	menuBtn.classList.toggle("open");
+
+	if (menuBtn.textContent === "☰") {
+		menuBtn.textContent = "✖";
+	} else {
+		menuBtn.textContent = "☰";
+	}
 });
