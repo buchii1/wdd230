e401c1bc750530931dbcfdd7b5051be186f65a05
buchii1 <oko22007@byui.edu>
2023-06-16 13:37:21 +0100

    Script to control the hamburger menu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,8 +1,18 @@
 // Get Current Year
-const dateToday = new Date();
-const year = dateToday.getFullYear();
+const year = new Date().getFullYear();
 document.querySelector("#year").textContent = year;
 
 // Get last modified date
 let lastModified = document.lastModified;
-document.querySelector("#lastModified").innerHTML = `Last Modification: ${lastModified}`;
+document.querySelector(
+	"#lastModified"
+).innerHTML = `Last Modification: ${lastModified}`;
+
+// Navigation
+const menuBtn = document.querySelector("#menu");
+const navUl = document.querySelector(".nav-ul");
+
+menuBtn.addEventListener("click", () => {
+	navUl.classList.toggle("open");
+	menuBtn.classList.toggle("open");
+});
