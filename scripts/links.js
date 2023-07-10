da92a3da3a8527e9acff06c376aef6a7c301b8f7
buchii1 <oko22007@byui.edu>
2023-07-10 16:20:25 +0100

    Dynamic links

--- scripts/links.js
+++ scripts/links.js
@@ -1,19 +1,31 @@
 const baseURL = "https://buchii1.github.io/wdd230/";
 const linksURL = "https://buchii1.github.io/wdd230/data/links.json";
+const ul = document.querySelector("#links");
 
 async function getLinks() {
-    try {
-        const response = await fetch(linksURL);
+	try {
+		const response = await fetch(linksURL);
 
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-        }
-        else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error);
-    }
+		if (response.ok) {
+			const data = await response.json();
+			console.log(data);
+			displayLinks(data.weeks);
+		} else {
+			throw Error(await response.text());
+		}
+	} catch (error) {
+		console.log(error);
+	}
 }
 getLinks();
+
+const displayLinks = (weeks) => {
+	let html = "";
+	weeks.forEach((week) => {
+		const weekHTML = week.links
+			.map((link) => `<a href="${link.url}">${link.title}</a>`)
+			.join(" | ");
+		html += `<li>${week.week}: ${weekHTML}</li>`;
+	});
+	ul.innerHTML = html;
+};
