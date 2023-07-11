7181ade2300ffce9b4541088582bdd5c6bebb56b
buchii1 <oko22007@byui.edu>
2023-07-11 11:39:59 +0100

    Added showGrid and showList function

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -31,3 +31,20 @@ const displayResults = (members) => {
 		display.innerHTML += section;
 	});
 };
+
+function showGrid() {
+	display.classList.remove("article-list");
+	display.classList.add("article-grid");
+	listButton.classList.remove("directory-active");
+	gridButton.classList.add("directory-active");
+}
+
+function showList() {
+	display.classList.remove("article-grid");
+	display.classList.add("article-list");
+	gridButton.classList.remove("directory-active");
+	listButton.classList.add("directory-active");
+}
+
+gridButton.addEventListener("click", showGrid);
+listButton.addEventListener("click", showList);
